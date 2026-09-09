3b0762df64fadb2de229f45968dd4a5b753a9222
aquistusfami <the.nv2418988@sis.hust.edu.vn>
2026-09-09 19:15:07 +0700

    feat(sway,waybar): set default 0 gaps for windows and full edge-to-edge waybar

--- .config/waybar/toggle-style.sh
+++ .config/waybar/toggle-style.sh
@@ -6,11 +6,13 @@ if [ -f "$STATE_FILE" ] && [ "$(cat "$STATE_FILE")" = "float" ]; then
     cp "$CONFIG_DIR/config-full.jsonc" "$CONFIG_DIR/config.jsonc"
     cp "$CONFIG_DIR/style-full.css" "$CONFIG_DIR/style.css"
     echo "full" > "$STATE_FILE"
+    echo "0" > "$HOME/.config/sway/.gaps_state"
     swaymsg gaps outer all set 0 && swaymsg gaps inner all set 0
 else
     cp "$CONFIG_DIR/config-float.jsonc" "$CONFIG_DIR/config.jsonc"
     cp "$CONFIG_DIR/style-float.css" "$CONFIG_DIR/style.css"
     echo "float" > "$STATE_FILE"
+    echo "5" > "$HOME/.config/sway/.gaps_state"
     swaymsg gaps outer all set 0 && swaymsg gaps inner all set 5
 fi
 
